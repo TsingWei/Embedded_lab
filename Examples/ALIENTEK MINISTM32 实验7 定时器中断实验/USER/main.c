9f71fc8936e92a1a296d9ab05f93753161547e93
moe <461626239@qq.com>
2019-09-17 18:43:20 +0800

    Reconstruct lab3

--- Examples/ALIENTEK MINISTM32 实验7 定时器中断实验/USER/main.c
+++ Examples/ALIENTEK MINISTM32 实验7 定时器中断实验/USER/main.c
@@ -11,7 +11,7 @@
 	delay_init();	    	 //延时函数初始化
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);// 设置中断优先级分组2
 	LED_Init();		  	//初始化与LED连接的硬件接口
-	TIM3_Int_Init(4999,7199);//10Khz的计数频率，计数到5000为500ms  
+	TIM3_Int_Init(999,7199);//10Khz的计数频率，计数到5000为500ms  
    	while(1)
 	{
 		LED0=!LED0;
